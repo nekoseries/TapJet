4f9586ca2b6ed9f6479f2dfc66c23c96687d44c7
Ivan Lianto <ivanlianto59@gmail.com>
2019-08-24 00:47:48 +0700

    Camera Movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public GameObject player;
+    public float speed = 5f;
+    public float smoothTime = 0.1f;
+
+    void Update()
+    {
+        Vector3 velocity = Vector3.zero;
+        if (Camera.main.WorldToScreenPoint(player.transform.position).y < 1 / 3f * Camera.main.pixelHeight || Camera.main.WorldToScreenPoint(player.transform.position).y > 2 / 3f * Camera.main.pixelHeight)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(0f, player.transform.position.y, -10f), ref velocity, smoothTime);
+            //transform.position = Vector3.MoveTowards(transform.position, new Vector3(0f, player.transform.position.y, -10f), speed * Time.deltaTime);
+        }
+    }
+}
